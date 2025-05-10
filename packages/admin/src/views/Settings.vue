cde7fd06e4d4b57cd9badae80b8330021a88104c
Alexander Vasilev <alexander.vasilev@protonmail.com>
2025-05-10 16:36:19 +0100

    some ideas for the settings page

--- packages/admin/src/views/Settings.vue
+++ packages/admin/src/views/Settings.vue
@@ -2,6 +2,9 @@
   <div class="min-h-screen bg-gray-100 p-6">
     <h1 class="text-3xl font-bold mb-6">Settings</h1>
     <router-link to="/" class="text-blue-600 hover:text-blue-800">← Back to Dashboard</router-link>
+    <p>Can cancel 2h before start of an event</p>
+    <p>Email templates</p>
+    <p>May be waiting list</p>
   </div>
 </template>
 
